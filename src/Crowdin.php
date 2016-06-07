8e7229d94f1e961c3f848194787e3a2452b0685f
Nikolai Plath <der.el.kuku@gmail.com>
2016-06-06 19:52:32 -0500

    Add base classes

---
+++ src/Crowdin.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Crowdin API implementation in PHP.
+ *
+ * @copyright  Copyright (C) 2016 Nikolai Plath (elkuku)
+ * @license    GNU General Public License version 2 or later
+ */
+
+namespace ElKuKu\Crowdin;
+
+use GuzzleHttp\Client as HttpClient;
+
+/**
+ * Class for interacting with Crowdin.
+ *
+ * @property-read  Package\File  $file  Crowdin API object for the file package.
+ *
+ * @since  1.0
+ */
+class Crowdin
+{
+	/**
+	 * @var string
+	 */
+	protected $projectId;
+
+	/**
+	 * @var string
+	 */
+	protected $apiKey;
+
+	/**
+	 * @var HttpClient
+	 */
+	protected $httpClient;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   string      $projectId  The project ID.
+	 * @param   string      $apiKey     The API key
+	 * @param   string      $baseUri    The base URI
+	 * @param   HttpClient  $client     The HTTP client object.
+	 *
+	 * @since    1.0
+	 */
+	public function __construct($projectId, $apiKey, $baseUri = 'http://api.crowdin.net/api/', HttpClient $client = null)
+	{
+		$this->projectId = $projectId;
+		$this->apiKey = $apiKey;
+
+		$this->httpClient  = isset($client) ? $client : new HttpClient(['base_uri' => $baseUri]);
+	}
+
+	/**
+	 * Magic method to lazily create API objects.
+	 *
+	 * @param   string  $name  Name of property to retrieve
+	 *
+	 * @return  Object
+	 *
+	 * @since   1.0
+	 * @throws  \InvalidArgumentException If $name is not a valid sub class.
+	 */
+	public function __get($name)
+	{
+		$class = 'ElKuKu\\Crowdin\\Package\\' . ucfirst($name);
+
+		if (class_exists($class))
+		{
+			if (false == isset($this->$name))
+			{
+				$this->$name = new $class($this->projectId, $this->apiKey, $this->httpClient);
+			}
+
+			return $this->$name;
+		}
+
+		throw new \InvalidArgumentException(sprintf('Argument %s produced an invalid class name: %s', $name, $class));
+	}
+}
---
+++ src/Package.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Crowdin API implementation in PHP.
+ *
+ * @copyright  Copyright (C) 2016 Nikolai Plath (elkuku)
+ * @license    GNU General Public License version 2 or later
+ */
+
+namespace ElKuKu\Crowdin;
+
+use GuzzleHttp\Client as HttpClient;
+
+/**
+ * Class Package
+ *
+ * @since  1.0
+ */
+abstract class Package
+{
+	/**
+	 * @var string
+	 */
+	private $projectId;
+
+	/**
+	 * @var string
+	 */
+	private $apiKey;
+
+	/**
+	 * @var HttpClient
+	 */
+	private $httpClient;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   string      $projectId   The project ID.
+	 * @param   string      $apiKey      The API key
+	 * @param   HttpClient  $httpClient  The HTTP client object.
+	 */
+	public function __construct($projectId, $apiKey, HttpClient $httpClient)
+	{
+		$this->projectId  = $projectId;
+		$this->apiKey     = $apiKey;
+		$this->httpClient = $httpClient;
+	}
+
+	/**
+	 * Get the project ID.
+	 *
+	 * @return string
+	 */
+	protected function getProjectId()
+	{
+		return $this->projectId;
+	}
+
+	/**
+	 * Get the API key.
+	 *
+	 * @return string
+	 */
+	protected function getApiKey()
+	{
+		return $this->apiKey;
+	}
+
+	/**
+	 * Get the HTTP client object.
+	 *
+	 * @return HttpClient
+	 */
+	protected function getHttpClient()
+	{
+		return $this->httpClient;
+	}
+}
