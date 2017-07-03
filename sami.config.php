53ea41720d2d8e2779d605070585624fe987fe5b
elkuku <der.el.kuku@gmail.com>
2017-07-03 14:35:03 -0500

    Update sami config file

--- sami.config.php
+++ sami.config.php
@@ -2,7 +2,7 @@
 
 return new Sami\Sami(
     'src', [
-        'title'                => 'Symfony2 API',
+        'title'     => 'Crowdin API',
         'build_dir' => 'build/api',
         'cache_dir' => 'build/cache',
     ]
