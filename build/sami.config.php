7c4cabfbb8bb5253c37462ad1f89124ec4e2434b
elkuku <der.el.kuku@gmail.com>
2017-07-03 12:02:50 -0500

    Add sami config file

---
+++ build/sami.config.php
@@ -0,0 +1,9 @@
+<?php
+
+return new Sami\Sami(
+    'src', [
+        'title'                => 'Symfony2 API',
+        'build_dir' => 'build/api',
+        'cache_dir' => 'build/cache',
+    ]
+);
