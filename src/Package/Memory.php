592508cccbfb861b7561df44cdb290356ab2961d
Denis Nikolas <design.by.niko.las@gmail.com>
2019-08-01 01:52:28 +0500

    Add source and terget lang options to downloadTM method

--- src/Package/Memory.php
+++ src/Package/Memory.php
@@ -25,16 +25,28 @@ Class Memory extends Package
 	 * @param   boolean  $includeAssigned  Defines whether the assigned TMs should be included in downloaded TMX file.
 	 *                                     Acceptable values are: 0, 1.
 	 *                                     Default is 1.
+	 * @param   boolean  $sourceLanguage Defines a source language for language pair. Сrowdin language code should be used.
+	 * @param   boolean  $targetLanguage Defines a target language for language pair. Сrowdin language code should be used.
 	 *
 	 * @since 1.0.5
 	 * @see   https://crowdin.com/page/api/download-tm
 	 *
 	 * @return ResponseInterface
 	 */
-	public function download(bool $includeAssigned = true) : ResponseInterface
+	public function download(bool $includeAssigned = true, string $sourceLanguage = '', string $targetLanguage = '') : ResponseInterface
 	{
+		$url = $this->getBasePath('download-tm');
+		if ($includeAssigned) {
+			$url .= '&include_assigned=1';
+		}
+		if (!empty($sourceLanguage)) {
+			$url .= '&source_language=' . $sourceLanguage;
+		}
+		if (!empty($targetLanguage)) {
+			$url .= '&target_language=' . $targetLanguage;
+		}
 		return $this->getHttpClient()
-			->get($this->getBasePath('download-tm') . '&include_assigned=' . (int) $includeAssigned);
+			->get($url);
 	}
 
 	/**
